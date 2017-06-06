0ebfe4e752d30454ff536c7ce107eb9d02db48a9
Daniel Weaver <danweaver06@gmail.com>
2017-06-06 18:50:30 -0400

    pass url as param

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use('/static', express.static('.'))
 var PORT = 3000;
 
 app.get('/summarize', function(req, res) {
+  console.log(req.query.url)
+  var url = req.query.url;
   request.get(
-    "https://api.aylien.com/api/v1/summarize?url=https%3A%2F%2Farstechnica.com%2Fapple%2F2017%2F06%2Fhomepod-is-apples-first-breakthrough-home-speaker&sentences_number=5"
+    "https://api.aylien.com/api/v1/summarize?=" + url
   )
   .set("X-AYLIEN-TextAPI-Application-ID", "17ee3853")
   .set("X-AYLIEN-TextAPI-Application-Key", "715b53a94c039fc008d65ed9f2ec9804")
--- summary.js
+++ summary.js
@@ -9,7 +9,7 @@ $(function() {
   'use strict';
 
 	$.ajax({
-    url: "http://localhost:3000/summarize",
+    url: "http://localhost:3000/summarize?url=https%3A%2F%2Farstechnica.com%2Fapple%2F2017%2F06%2Fhomepod-is-apples-first-breakthrough-home-speaker&sentences_number=5",
     headers: {"X-AYLIEN-TextAPI-Application-Key": "715b53a94c039fc008d65ed9f2ec9804",
     					"X-AYLIEN-TextAPI-Application-ID": "17ee3853" },
     success: callback
